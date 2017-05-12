e3dc2741042b04e592d79464a93beb31507a76fd
zcube <rizwanbpatel@gmail.com>
2017-05-12 15:58:06 +0530

    Working List Detail Fragment module

--- app/src/main/java/com/example/android/rssreader/RssfeedActivity.java
+++ app/src/main/java/com/example/android/rssreader/RssfeedActivity.java
@@ -3,7 +3,12 @@ package com.example.android.rssreader;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class RssfeedActivity extends Activity {
+public class RssfeedActivity extends Activity implements MyListFragment.OnItemSelectedListener{
+    @Override
+    public void onRssItemSelected(String link) {
+        DetailFragment fragment = (DetailFragment)getFragmentManager().findFragmentById(R.id.detailFragment);
+        fragment.setText(link);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
