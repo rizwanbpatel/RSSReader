a6b4c609e71fca0c2131f99b193faf44c8ef9f5e
Rizwan Patel <rizwan.patel@tomtom.com>
2017-05-12 14:43:47 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/rssreader/RssfeedActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.rssreader;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RssfeedActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
